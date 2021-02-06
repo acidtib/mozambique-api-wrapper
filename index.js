4baa4623041095769c77f90c207552f798a1c51e
Fernando <fernandopach2017@gmail.com>
2021-02-05 23:23:46 -0300

    Add npm test, add more typedefs

--- index.js
+++ index.js
@@ -10,7 +10,175 @@ const DIRECTORY = {
   GAME_DATA: BASE_URL + "/gamedata?"
 };
 
-//#region Typedefs
+//#region Private functions
+/**
+ * Make the request to the API servers
+ * 
+ * @private
+ * @param {*} self
+ * @param {String} url
+ */
+function request(self, url) {
+  return fetch(url.replace(/\{v\}/g, self.version), {
+    headers: self.headers
+  })
+  .then(function (res) {
+    return res.json();
+  })
+  .catch(function (err) {
+    return Promise.reject(err);
+  });
+}
+//#endregion
+
+
+/**
+ * Core of mozambique-api-wrapper
+ * 
+ * @constructor
+ * @param {String} apiKey Your [Apex Legends API](https://apexlegendsapi.com) Auth Key
+ * @param {Number} [version=5] API version to use
+ */
+class MozambiqueAPI {
+  constructor(apiKey, version = 5) {
+    if (!apiKey) throw new Error("[ERROR] mozampique-api-wrapper: API Key missing");
+
+    let self = this;
+    self.apiKey = apiKey;
+    self.version = version;
+    self.headers = {
+      "User-Agent": "mozambique-api-wrapper",
+      "Content-Type": "application/json",
+      "Authorization": self.apiKey
+    };
+  }
+
+  /**
+   * Search a player using player name or UID
+   *
+   * @param {Object} query - Query parameters
+   * @param {String} [query.player] - Player name
+   * @param {String|Number} [query.uid] - Player UID
+   * @param {String} [query.platform] - Player platform
+   * @returns {Player} Object with player info
+   */
+  search(query) {
+    let type;
+    if (query.player) type = "player=" + query.player;
+    if (query.uid) type = "uid=" + query.uid;
+    let url = DIRECTORY.SEARCH_URL + "&platform=" + query.platform + "&" + type;
+    return request(this, url);
+  }
+
+  /**
+   * Get recent news about Apex Legends
+   *
+   * @param {String} [lang="en-us"] Language of the news
+   * @returns {Array} Array of Apex Legends news
+   */
+  news(lang = "en-us") {
+    let url = DIRECTORY.NEWS_URL + "lang=" + lang;
+    return request(this, url);
+  }
+
+  /**
+   * Get server status for Origin, EA, Apex Legends and apexlegendsapi API
+   *
+   * @returns {Object} Object with status of all servers
+   */
+  server() {
+    let url = DIRECTORY.SERVER_STATUS;
+    return request(this, url);
+  }
+
+  /**
+   * Avaliable for everyone but with limitations depending on your api access type
+   *
+   * @param {String} action - Action for the Match History API (info, get, delete, add)
+   * @param {Object} [query] - Query parameters
+   * @param {String} [query.player] - Player name
+   * @param {String|Number} [query.uid] - Player UID
+   * @param {String} [query.platform] - Player platform
+   * @returns {Object} Data returned differs depending on action parameter. Please refer to [API documentation](https://apexlegendsapi.com) for more info 
+   */
+  history(action, query) {
+    let q = ''; 
+    if(action != "info") {
+      let type;
+      if (query.player) type = "player=" + query.player;
+      if (query.uid) type = "uid=" + query.uid;
+      q = type + "&platform=" + query.platform + "&";
+    }
+
+    let url = DIRECTORY.MATCH_HISTORY + q + "history=1&action=" + action;
+    return request(this, url);
+  }
+
+  
+  /**
+   * Compare two players
+   *
+   * @param {Object} query1 - Query parameters
+   * @param {String} [query1.player] - Player name
+   * @param {String|Number} [query1.uid] - Player UID
+   * @param {String} [query1.platform] - Player platform
+   * @param {Object} query2 - Query parameters
+   * @param {String} [query2.player] - Player name
+   * @param {String|Number} [query2.uid] - Player UID
+   * @param {String} [query2.platform] - Player platform
+   * @returns {ComparedPlayers}
+   */
+  async compare(query1, query2) {
+    if(!query1.platform || !query2.platform) throw new Error("Platform required");
+    
+    var DataObj = {
+      players: [],
+      comparedData: {}
+    };
+
+    if(query1.platform == query2.platform) {
+      let type;
+      if (query1.player) type = `player=${query1.player},${query2.player}`;
+      if (query1.uid) type = `uid=${query1.uid},${query2.uid}`;
+      let url = DIRECTORY.SEARCH_URL + "&platform=" + query1.platform + "&" + type;
+      DataObj.players = await request(this, url);
+    } else {
+      
+      let type1;
+      if (query1.player) type1 = "player=" + query1.player;
+      if (query1.uid) type1 = "uid=" + query1.uid;
+      let url1 = DIRECTORY.SEARCH_URL + "&platform=" + query1.platform + "&" + type1;
+      DataObj.players[0] = await request(this, url1);
+
+      let type2;
+      if (query2.player) type2 = "player=" + query2.player;
+      if (query2.uid) type2 = "uid=" + query2.uid;
+      let url2 = DIRECTORY.SEARCH_URL + "&platform=" + query2.platform + "&" + type2;
+      DataObj.players[1] = await request(this, url2);
+    }
+
+    return DataObj;
+  }
+
+  /**
+   * WARNING: endpoint data not updated anymore
+   * 
+   * Avaliable data types:
+   * assault_rifles, attachments, consumables, equipment, grenades, legends, light_machine_guns, pistols, shotguns, sniper_rifles, sub_machine_guns
+   * @deprecated data not update anymore
+   * @param {String} dataType Type of data requested
+   * @returns {Object} Object with requested game data
+   */
+  gamedata(dataType) {
+    let url = DIRECTORY.GAME_DATA + "type=" + dataType;
+    return request(this, url);
+  }
+}
+
+module.exports = MozambiqueAPI;
+
+
+//#region JSDoc typedefs
 /**
  * Legend object
  * @typedef {Object} Legend
@@ -104,166 +272,13 @@ const DIRECTORY = {
  * 
  * @property {Object} total
  */
+
+ /**
+  * Apex Legends News Object
+  * @typedef {Object} ApexNews
+  * @property {String} title
+  * @property {String} link
+  * @property {String} img
+  * @property {String} short_desc
+  */
 //#endregion
-
-//#region Private functions
-/**
- * Make the request to the API servers
- * 
- * @private
- * @param {*} self
- * @param {String} url
- */
-function request(self, url) {
-  return fetch(url.replace(/\{v\}/g, self.version), {
-    headers: self.headers
-  })
-  .then(function (res) {
-    return res.json();
-  })
-  .catch(function (err) {
-    return Promise.reject(err);
-  });
-}
-//#endregion
-
-
-/**
- * Core of mozambique-api-wrapper
- * 
- * @constructor
- * @param {String} apiKey Your [Apex Legends API](https://apexlegendsapi.com) Auth Key
- * @param {Number} [version=5] API version to use
- */
-class MozambiqueAPI {
-  constructor(apiKey, version = 5) {
-    if (!apiKey) throw new Error("[ERROR] mozampique-api-wrapper: API Key missing");
-
-    let self = this;
-    self.apiKey = apiKey;
-    self.version = version;
-    self.headers = {
-      "User-Agent": "mozambique-api-wrapper",
-      "Content-Type": "application/json",
-      "Authorization": self.apiKey
-    };
-  }
-
-  /**
-   * Search a player using player name or UID
-   *
-   * @param {Object} query - Query parameters
-   * @param {String} [query.player] - Player name
-   * @param {String|Number} [query.uid] - Player UID
-   * @param {String} [query.platform] - Player platform
-   * @returns {Player} Object with player info
-   */
-  search(query) {
-    let type;
-    if (query.player) type = "player=" + query.player;
-    if (query.uid) type = "uid=" + query.uid;
-    let url = DIRECTORY.SEARCH_URL + "&platform=" + query.platform + "&" + type;
-    return request(this, url);
-  }
-
-  /**
-   * Get recent news about Apex Legends
-   *
-   * @param {String} [lang="en-us"] Language of the news
-   * @returns {Array} Array of Apex Legends news
-   */
-  news(lang = "en-us") {
-    let url = DIRECTORY.NEWS_URL + "lang=" + lang;
-    return request(this, url);
-  }
-
-  /**
-   * Get server status for Origin, EA, Apex Legends and apexlegendsapi API
-   *
-   * @returns {Object} Object with status of all servers
-   */
-  server() {
-    let url = DIRECTORY.SERVER_STATUS;
-    return request(this, url);
-  }
-
-  /**
-   * Avaliable for everyone but with limitations depending on your api access type
-   *
-   * @param {Object} query - Query parameters
-   * @param {String} [query.player] - Player name
-   * @param {String|Number} [query.uid] - Player UID
-   * @param {String} [query.platform] - Player platform
-   * @param {String} action - Action for the Match History API (info, get, delete, add)
-   * @returns {Object} Data returned differs depending on action parameter. Please refer to [API documentation](https://apexlegendsapi.com) for more info 
-   */
-  history(query, action) {
-    let type;
-    if (query.player) type = "player=" + query.player;
-    if (query.uid) type = "uid=" + query.uid;
-    let url = DIRECTORY.MATCH_HISTORY + type + "&platform" + query.platform + "&history=1&action=" + action;
-    return request(this, url);
-  }
-
-  
-  /**
-   * Compare two players
-   *
-   * @param {Object} query1 - Query parameters
-   * @param {String} [query1.player] - Player name
-   * @param {String|Number} [query1.uid] - Player UID
-   * @param {String} [query1.platform] - Player platform
-   * @param {Object} query2 - Query parameters
-   * @param {String} [query2.player] - Player name
-   * @param {String|Number} [query2.uid] - Player UID
-   * @param {String} [query2.platform] - Player platform
-   * @returns {ComparedPlayers}
-   */
-  async compare(query1, query2) {
-    if(!query1.platform || !query2.platform) throw new Error("Platform required");
-    
-    var DataObj = {
-      players: [],
-      comparedData: {}
-    };
-
-    if(query1.platform == query2.platform) {
-      let type;
-      if (query1.player) type = `player=${query1.player},${query2.player}`;
-      if (query1.uid) type = `uid=${query1.uid},${query2.uid}`;
-      let url = DIRECTORY.SEARCH_URL + "&platform=" + query1.platform + "&" + type;
-      DataObj.players = await request(this, url);
-    } else {
-      
-      let type1;
-      if (query1.player) type1 = "player=" + query1.player;
-      if (query1.uid) type1 = "uid=" + query1.uid;
-      let url1 = DIRECTORY.SEARCH_URL + "&platform=" + query1.platform + "&" + type1;
-      DataObj.players[0] = await request(this, url1);
-
-      let type2;
-      if (query2.player) type2 = "player=" + query2.player;
-      if (query2.uid) type2 = "uid=" + query2.uid;
-      let url2 = DIRECTORY.SEARCH_URL + "&platform=" + query2.platform + "&" + type2;
-      DataObj.players[1] = await request(this, url2);
-    }
-
-    return DataObj;
-  }
-
-  /**
-   * WARNING: endpoint data not updated anymore
-   * 
-   * Avaliable data types:
-   * assault_rifles, attachments, consumables, equipment, grenades, legends, light_machine_guns, pistols, shotguns, sniper_rifles, sub_machine_guns
-   * @deprecated data not update anymore
-   * @param {String} dataType Type of data requested
-   * @returns {Object} Object with requested game data
-   */
-  gamedata(dataType) {
-    let url = DIRECTORY.GAME_DATA + "type=" + dataType;
-    return request(this, url);
-  }
-}
-
-module.exports = MozambiqueAPI;
--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,91 @@
 const MozambiqueAPI = require("../index.js");
+const fs = require("fs");
+
+const TEST_TOKEN = "test token here";
+
+if (TEST_TOKEN == "test token here") {
+  console.warn("Please provide an API token for testing");
+  process.exit(1);
+}
+
+var client = new MozambiqueAPI(TEST_TOKEN);
+
+function newError(err, method) {
+  if (!fs.existsSync("testErrors")) fs.mkdirSync("testErrors");
+  let logName = `testErrors/${method}.json`;
+  fs.writeFileSync(logName, JSON.stringify(err, null, 2), {
+    encoding: "utf8",
+  });
+  console.log(`Error log created at ${logName}`);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function test() {
+  client
+    .search({ player: "KingBR", platform: "PC" })
+    .then((resp) => {
+      console.log("Search: OK");
+    })
+    .catch((err) => {
+      console.log("Search: ERROR");
+      newError(err, "search");
+    });
+
+  await sleep(1000);
+
+  client
+    .news()
+    .then((resp) => {
+      console.log("News: OK");
+    })
+    .catch((err) => {
+      console.log("News: ERROR");
+      newError(err, "news");
+    });
+
+  await sleep(1000);
+
+  client
+    .server()
+    .then((resp) => {
+      console.log("Server: OK");
+    })
+    .catch((err) => {
+      console.log("Server: ERROR");
+      newError(err, "server");
+    });
+
+  await sleep(1000);
+
+  client
+    .history("info")
+    .then((resp) => {
+      console.log("History: OK");
+    })
+    .catch((err) => {
+      console.log("History: ERROR");
+      newError(err, "history");
+    });
+
+  await sleep(1000);
+
+  client
+    .compare(
+      { player: "KingBR", platform: "PC" },
+      { player: "HeyImLIFELINE", platform: "PC" }
+    )
+    .then((resp) => {
+      console.log("Compare: OK");
+    })
+    .catch((err) => {
+      console.log("Compare: ERROR");
+      newError(err, "compare");
+    });
+
+  process.exit(0);
+}
+
+test();
